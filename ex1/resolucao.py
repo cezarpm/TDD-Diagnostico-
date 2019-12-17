c364fd8e1e6f69cf0a5acd46ff8cb329450c615d
unknown <cezar_pm@hotmail.com>
2019-12-17 19:18:53 -0300

    Resolução do primeiro exercicio

---
+++ ex1/resolucao.py
@@ -0,0 +1,9 @@
+frase = input('aaaaaa: ')
+
+for i in range(7):
+    for i in range(6):
+        print(frase, end=" ")
+
+    print(frase)
+
+input('Digite alguma coisa para encerrar o programa ')   
